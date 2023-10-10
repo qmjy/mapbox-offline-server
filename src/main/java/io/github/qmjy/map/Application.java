bc45ff6527857cb348b45f266b96d7dbe45f7b11
liushaofeng <liushaofeng89@qq.com>
2023-10-10 23:35:59 +0800

    修复服务启动错误

--- src/main/java/io/github/qmjy/map/Application.java
+++ src/main/java/io/github/qmjy/map/Application.java
@@ -3,6 +3,10 @@ package io.github.qmjy.map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序启动入口
+ * @author Shaofeng Liu
+ */
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {

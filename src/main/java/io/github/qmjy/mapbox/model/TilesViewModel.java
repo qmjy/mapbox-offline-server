1cbee39ac0a084ddc14b25ffee8eb67c4d986041
Shaofeng Liu <liushaofeng89@qq.com>
2024-01-07 11:15:32 +0800

    format tilesets list page content

--- src/main/java/io/github/qmjy/mapbox/model/TilesViewModel.java
+++ src/main/java/io/github/qmjy/mapbox/model/TilesViewModel.java
@@ -20,20 +20,57 @@ import java.io.File;
 
 public class TilesViewModel {
     private final String name;
+    private final String type;
+    private final String fileSize;
 
     /**
      * 构造方法
+     *
      * @param file 瓦片文件
      */
     public TilesViewModel(File file) {
-        this.name = file.getName();
+        String fileName = file.getName();
+        this.name = fileName;
+        this.type = file.isDirectory() ? "" : fileName.substring(fileName.lastIndexOf("."));
+        this.fileSize = file.isDirectory() ? "" : formatFileSize(file.length());
     }
 
     /**
+     * 文件大小智能转换
+     * 会将文件大小转换为最大满足单位
+     *
+     * @param size（文件大小，单位为B）
+     * @return 文件大小
+     */
+    public static String formatFileSize(Long size) {
+        String sizeName = null;
+        if (1024 * 1024 > size && size >= 1024) {
+            sizeName = String.format("%.2f", size.doubleValue() / 1024) + "KB";
+        } else if (1024 * 1024 * 1024 > size && size >= 1024 * 1024) {
+            sizeName = String.format("%.2f", size.doubleValue() / (1024 * 1024)) + "MB";
+        } else if (size >= 1024 * 1024 * 1024) {
+            sizeName = String.format("%.2f", size.doubleValue() / (1024 * 1024 * 1024)) + "GB";
+        } else {
+            sizeName = size.toString() + "B";
+        }
+        return sizeName;
+    }
+
+
+    /**
      * 获取瓦片文件名称
+     *
      * @return 瓦片文件名称
      */
     public String getName() {
         return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getFileSize() {
+        return fileSize;
     }
 }

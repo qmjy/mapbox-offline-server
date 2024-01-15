85ad16f50306b491cdc20afb8889e04d2075fc6e
Shaofeng Liu <liushaofeng89@qq.com>
2024-01-15 21:33:37 +0800

    地理逆编码新增行政区划分割符

--- src/main/java/io/github/qmjy/mapbox/controller/MapServerGeoController.java
+++ src/main/java/io/github/qmjy/mapbox/controller/MapServerGeoController.java
@@ -71,12 +71,14 @@ public class MapServerGeoController {
      *
      * @param location 经度在前，纬度在后，经纬度间以“,”分割，经纬度小数点后不要超过 6 位。
      * @param langType 可选参数，支持本地语言(0:default)和英语(1)。
+     * @param splitter 各行政区划节点之间的分割符。
      * @return 地理逆编码结果
      */
     @Operation(summary = "地理逆编码查询", description = "通过经纬度查询行政区划概要信息，通过区划ID可获取行政区划详细信息。")
     @GetMapping("regeo")
     public ResponseEntity<Map<String, Object>> regeo(@Parameter(description = "待查询的经纬度坐标，例如：104.071883,30.671974") @RequestParam(value = "location") String location,
-                                                     @Parameter(description = "返回的数据语言。0：本地语言（default）；1：英语") @RequestParam(value = "langType", required = false, defaultValue = "0") int langType) {
+                                                     @Parameter(description = "返回的数据语言。0：本地语言（default）；1：英语") @RequestParam(value = "langType", required = false, defaultValue = "0") int langType,
+                                                     @Parameter(description = "各行政区划节点之间的分割符。默认本地语言无分隔符，英文为空格。") @RequestParam(value = "splitter", required = false, defaultValue = "") String splitter) {
         Map<Integer, List<SimpleFeature>> administrativeDivisionLevel = MapServerDataCenter.getAdministrativeDivisionLevel();
         if (administrativeDivisionLevel.isEmpty()) {
             String msg = "Can't find any geojson file for boundary search!";
@@ -95,7 +97,7 @@ public class MapServerGeoController {
                     GeometryBuilder geometryBuilder = new GeometryBuilder();
                     Point point = geometryBuilder.point(Double.parseDouble(split[0]), Double.parseDouble(split[1]));
                     if (polygon.covers(point)) {
-                        String parentPath = getParentFullPath(simpleFeature, langType);
+                        String parentPath = getParentFullPath(simpleFeature, langType, splitter);
 
                         HashMap<Object, Object> data = new HashMap<>();
                         data.put("id", simpleFeature.getAttribute("osm_id"));
@@ -112,16 +114,16 @@ public class MapServerGeoController {
         return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(ResponseMapUtil.notFound());
     }
 
-    private String getParentFullPath(SimpleFeature simpleFeature, int langType) {
+    private String getParentFullPath(SimpleFeature simpleFeature, int langType, String splitter) {
         String parentsString = (String) simpleFeature.getAttribute("parents");
         String[] parents = parentsString.split(",");
         StringBuilder sb = new StringBuilder();
         for (int j = parents.length - 1; j >= 0; j--) {
             SimpleFeature feature = MapServerDataCenter.getAdministrativeDivision().get(Integer.parseInt(parents[j]));
-            if (langType == 0) {
-                sb.append(feature.getAttribute("local_name"));
+            if (splitter == null || splitter.trim().isEmpty()) {
+                sb.append(langType == 0 ? feature.getAttribute("local_name") : feature.getAttribute("name_en") + " ");
             } else {
-                sb.append(feature.getAttribute("name_en")).append(" ");
+                sb.append(langType == 0 ? feature.getAttribute("local_name") : feature.getAttribute("name_en")).append(splitter);
             }
         }
         return sb.toString();

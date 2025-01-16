c488f8e314242cf911d87cb80db0a815b1765104
liushaofeng <liushaofeng@qq.com>
2025-01-16 11:10:10 +0800

    add api for poi search

--- src/main/java/io/github/qmjy/mapserver/controller/MapServerPOIRestController.java
+++ src/main/java/io/github/qmjy/mapserver/controller/MapServerPOIRestController.java
@@ -59,22 +59,60 @@ public class MapServerPOIRestController {
     /**
      * POI搜索
      *
+     * @param keywords POI关键字。目前只支持单个关键词
+     * @param pageSize 返回的结果条数
+     * @return 查询到到的POI搜索结果
+     */
+    @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @Operation(summary = "获取POI数据", description = "查询POI数据。")
+    public ResponseEntity<Map<String, Object>> poiSearch(@Parameter(description = "待查询POI关键字，目前只支持一个关键词") @RequestParam String keywords, @Parameter(description = "返回的POI结果条数,取值范围为1-100") @RequestParam(required = false, defaultValue = "10") int pageSize) {
+        if (keywords.trim().isEmpty() || keywords.split(" ").length > 1) {
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(ResponseMapUtil.notFound("参数不合法，请检查参数！"));
+        }
+
+        File poiIndexFile = getPoiFile();
+        if (poiIndexFile.exists()) {
+            JdbcTemplate idxJdbcTemp = JdbcUtils.getInstance().getJdbcTemplate(appConfig.getDriverClassName(), poiIndexFile.getAbsolutePath());
+            String sql = "SELECT * FROM poi WHERE name LIKE ? LIMIT " + (pageSize <= 0 || pageSize > 100 ? 10 : pageSize);
+            List<Map<String, Object>> maps = idxJdbcTemp.queryForList(sql, "%" + keywords + "%");
+            List<PoiPoint> dataList = new ArrayList<>();
+            maps.forEach(stringObjectMap -> {
+                dataList.add(new PoiPoint((String) stringObjectMap.get("name"), stringObjectMap.get("lon") + "," + stringObjectMap.get("lat")));
+            });
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(ResponseMapUtil.ok(dataList));
+        } else {
+            return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(ResponseMapUtil.notFound("Can't find POI data or POI index service not ready yet!"));
+        }
+    }
+
+    private File getPoiFile() {
+        File[] files = new File(appConfig.getDataPath() + File.separator + "poi" + File.separator).listFiles((dir, name) -> name.endsWith(".poi"));
+        if (files != null && files.length > 0) {
+            return files[0];
+        } else {
+            logger.error("Can't find POI data file!");
+            return new File("");
+        }
+    }
+
+    /**
+     * POI搜索
+     *
      * @param poiFile  待查询的瓦片数据文件名或POI文件名，例如chengdu.mbtiles或者chengdu.poi
      * @param keywords POI关键字。目前只支持单个关键词
+     * @param pageSize 返回的结果条数
      * @return 查询到到的POI搜索结果
      */
     @GetMapping(value = "/{poiIndexFile}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    @Operation(summary = "获取POI数据", description = "查询POI数据。")
-    public ResponseEntity<Map<String, Object>> loadJpegTile(@Parameter(description = "查询POI数据的矢量瓦片数据源或POI文件名，例如：Chengdu.mbtiles | Chengdu.poi") @PathVariable("poiIndexFile") String poiFile,
-                                                            @Parameter(description = "待查询POI关键字，目前只支持一个关键词") @RequestParam String keywords,
-                                                            @Parameter(description = "返回的POI结果条数,取值范围为1-100") @RequestParam(required = false, defaultValue = "10") int pageSize) {
+    @Operation(summary = "获取POI数据", description = "指定检索文件来查询POI数据。")
+    public ResponseEntity<Map<String, Object>> poiSearchByFile(@Parameter(description = "查询POI数据的矢量瓦片数据源或POI文件名，例如：Chengdu.mbtiles | Chengdu.poi") @PathVariable("poiIndexFile") String poiFile, @Parameter(description = "待查询POI关键字，目前只支持一个关键词") @RequestParam String keywords, @Parameter(description = "返回的POI结果条数,取值范围为1-100") @RequestParam(required = false, defaultValue = "10") int pageSize) {
         if (keywords.trim().isEmpty() || keywords.split(" ").length > 1 || SystemUtils.checkTilesetName(poiFile)) {
             return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(ResponseMapUtil.notFound("参数不合法，请检查参数！"));
         }
 
-        String filePath = poiFile.endsWith(".poi") ? appConfig.getDataPath() + File.separator + "poi" + File.separator
-                + poiFile : appConfig.getDataPath() + File.separator + "tilesets" + File.separator + poiFile + ".idx";
+        String filePath = poiFile.endsWith(".poi") ? appConfig.getDataPath() + File.separator + "poi" + File.separator + poiFile : appConfig.getDataPath() + File.separator + "tilesets" + File.separator + poiFile + ".idx";
         File poiIndexFile = new File(filePath);
         if (poiIndexFile.exists()) {
             JdbcTemplate idxJdbcTemp = JdbcUtils.getInstance().getJdbcTemplate(appConfig.getDriverClassName(), filePath);

d302d64394fdf6708421bd7519502cdcb1ba2fc8
liushaofeng <liushaofeng@qq.com>
2025-10-17 00:00:20 +0800

    update README and Migration project to Chilo

--- src/main/java/io/github/qmjy/mapserver/util/SystemUtils.java
+++
@@ -1,7 +0,0 @@
-package io.github.qmjy.mapserver.util;
-
-public class SystemUtils {
-    public static boolean checkTilesetName(String tileset) {
-        return tileset.contains("..") || tileset.contains("/") || tileset.contains("\\");
-    }
-}
